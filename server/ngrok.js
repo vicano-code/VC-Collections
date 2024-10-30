fbcad32b847f1e0471dfd5755f7279d10651b91b
Victor Anokwuru <vc.anokwuru@gmail.com>
2024-10-30 09:27:07 +0100

    render port update in ngrok connection file

--- server/ngrok.js
+++ server/ngrok.js
@@ -3,9 +3,9 @@ import ngrok from 'ngrok';
 (async function () {
   try {
     const url = await ngrok.connect({
-      proto: 'http', // or https if your app is secure
-      addr: process.env.PORT || 5000, // your Render app's port
-      authtoken: process.env.NGROK_AUTH_TOKEN, // your ngrok auth token
+      proto: 'https',
+      addr: process.env.PORT || 10000, // Render app's port
+      authtoken: process.env.NGROK_AUTH_TOKEN, // ngrok auth token
       region: 'us',
     });
     console.log(`ngrok tunnel established at: ${url}`);
